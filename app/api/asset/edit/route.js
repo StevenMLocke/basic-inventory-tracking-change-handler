60bbabc26d769ff26b6b05fec20d12220d21ee50
Steven M. Locke <mr.locke@sandandorsnow.com>
2023-10-19 08:02:14 -0400

    fixed asset editing.

--- app/api/asset/edit/route.js
+++ app/api/asset/edit/route.js
@@ -4,12 +4,14 @@ import { NextResponse } from "next/server";
 export async function POST(req) {
 	const { asset } = await req.json()
 
+	//the following are part of the json payload but not needed in the sql query.
 	delete asset.model
 	delete asset.manufacturer
 	delete asset.location_name
 	delete asset.status_name
 	delete asset.user_email
 	delete asset.funding_source_name
+	delete asset.category
 
 	if (asset.purchase_date) {
 		asset.purchase_date = new Date(asset.purchase_date)
